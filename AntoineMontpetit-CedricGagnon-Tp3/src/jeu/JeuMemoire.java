8a429644bac380985aebe56c2d955c33d8d3126c
Inlinkcraft <47537121+Inlinkcraft@users.noreply.github.com>
2021-03-03 15:09:19 -0500

    Jeu Memoire

---
+++ AntoineMontpetit-CedricGagnon-Tp3/src/jeu/JeuMemoire.java
@@ -0,0 +1,69 @@
+package jeu;
+
+import java.util.ArrayList;
+import java.awt.Point;
+import formes.Forme;
+
+public class JeuMemoire implements Memorisable{
+
+	public static final int LIGNE = 6;
+	public static final int COLONNE = 6;
+	public static final int NBR_ELEMENTS_GRILLE = LIGNE * COLONNE;
+	private static final int LONGUEUR_CHAINE = 17;
+
+	private ArrayList<Point> vecteurPoints;
+	private int niveau;
+
+	public JeuMemoire() {
+
+	}
+
+	private void preparerVecteurFormes() {
+
+	}
+
+	private void preparerGrilleDeJeu() {
+
+	}
+	
+	public String toString() {
+		
+	}
+	
+	private String ajouterEspaces(int pLongueur, String pNom) {
+		
+	}
+	
+	public String getNomForme(int pX, int pY) {
+		
+	}
+	
+	public ArrayList<Point> jouerOrdi(){
+		
+	}
+	
+	private Point choisirForme() {
+		
+	}
+	
+	public boolean jouerHumain(int pX, int pY) {
+		
+	}
+	
+	public int getNiveau() {
+		
+	}
+	
+	public VecteurFormes getVecteur() {
+		
+	}
+	
+	public Forme[][] getGrille(){
+		
+	}
+	
+	public void setNiveauPlusUn() {
+		
+	}
+
+}
