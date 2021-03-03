864f3945c3ed56301a1c4696b5c0fa19b49d1bf0
Inlinkcraft <47537121+Inlinkcraft@users.noreply.github.com>
2021-03-03 15:54:29 -0500

    Constructor

--- AntoineMontpetit-CedricGagnon-Tp3/src/jeu/JeuMemoire.java
+++ AntoineMontpetit-CedricGagnon-Tp3/src/jeu/JeuMemoire.java
@@ -12,19 +12,29 @@ public class JeuMemoire implements Memorisable{
 	public static final int NBR_ELEMENTS_GRILLE = LIGNE * COLONNE;
 	private static final int LONGUEUR_CHAINE = 17;
 
+	private VecteurFormes vecteurFormes;
+	private Forme[][] grilleDeJeu;
 	private ArrayList<Point> vecteurPoints;
 	private int niveau;
 
 	public JeuMemoire() {
-
+		preparerVecteurFormes();
+		preparerGrilleDeJeu();
 	}
 
 	private void preparerVecteurFormes() {
-
+		vecteurFormes = new VecteurFormes();
+		vecteurFormes.melanger();
 	}
 
 	private void preparerGrilleDeJeu() {
-
+		grilleDeJeu = new Forme[LIGNE][COLONNE];
+		ArrayList<Forme> listFormes = vecteurFormes.getVecteur();
+		for(int ligne = 0; ligne < LIGNE; ligne++) {
+			for(int colonne = 0; colonne < COLONNE; colonne++) {
+				grilleDeJeu[ligne][colonne] = listFormes.get(ligne*colonne + colonne);
+			}
+		}
 	}
 	
 	public String toString() {
@@ -60,7 +70,7 @@ public class JeuMemoire implements Memorisable{
 	}
 	
 	public Forme[][] getGrille(){
-		
+		return grilleDeJeu;
 	}
 	
 	public void setNiveauPlusUn() {
